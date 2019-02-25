a902ffffb913e131a1dc19b2ad6c986c9901510c
NahokoNishijima <naho_kokko@yahoo.co.jp>
2019-02-25 21:29:23 +0900

    Firstkadaiappのラベルを追加した。

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 西島菜穂子 on 2019/02/24.
+//  Copyright © 2019 nahoko.nishijima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
